0a075b24d3032e9f05b7aa1d7c1be56bba597051
kinan.tassapehji <kinan.tassapehji@users.noreply.github.com>
2020-09-15 12:12:30 +0300

    Second Comment After Waleed Comment

--- BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -7,6 +7,7 @@ namespace BlazorMovies.Shared.Entities
 {
     public class Movie
     {
+        //Second Comment
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
